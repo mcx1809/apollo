cd740a3a7b337933bfd165be8cc252fee6d9db04
jian_liu <514695412@qq.com>
2018-09-27 11:02:05 +0800

    refactor lmpackmsg_generator.py according to code style

--- modules/tools/localization/lmpackmsg_generator.py
+++ modules/tools/localization/lmpackmsg_generator.py
@@ -12,29 +12,35 @@ def read_bag(filename):
 	obs_list  = []      # obstacle message list
 	odo_list  = []      # odometry message list
 	bag = rosbag.Bag(filename)
-	for topic, msg, t in bag.read_messages(topics=['/apollo/perception/obstacles']):
+	for topic, msg, t in bag.read_messages(topics
+		=['/apollo/perception/obstacles']):
 		obs_list.append(msg)
-	for topic, msg, t in bag.read_messages(topics=['/apollo/sensor/gnss/odometry']):
+	for topic, msg, t in bag.read_messages(topics
+		=['/apollo/sensor/gnss/odometry']):
 		odo_list.append(msg)
 	bag.close()
 	return (obs_list,odo_list)
 
 def write_info(source_tuple, filename):
-	"""1. acquire the correct gps msg from odometrymsg according to the timestamp from obstaclemsg 
+	"""1. acquire the correct gps msg from odometrymsg according to the 
+		  timestamp from obstaclemsg 
 	   2. set the value of properties of OdometryLaneMarker message
 	   3. group the OdometryLaneMarker message to ContourOdometryLaneMarkers
-	   4. set the property value of OdometryLaneMarkersPack according to ContourOdometryLaneMarkers
-	   4. write the serialized string of OdometryLaneMarkersPack message to file
+	   4. set the property value of OdometryLaneMarkersPack according 
+	   	  to ContourOdometryLaneMarkers
+	   5. write the serialized string of OdometryLaneMarkersPack message to file
 	Args: 
-		tuple:(obstaclemsg_list, odometrymsg_list)
+		source_tuple:(obstaclemsg_list, odometrymsg_list)
 		filename: file to store protobuf msg
 	"""
 	f = open(filename, "wb")
 	odometry_lane_markers_pack = odometry_lane_marker_pb2.OdometryLaneMarkersPack()
 	left_lane_marker_group  = odometry_lane_markers_pack.lane_markers.add()
 	right_lane_marker_group = odometry_lane_markers_pack.lane_markers.add()
-	for obs_index in range(len(source_tuple[0])):                 # each message in obstacle message list
-		for odo_index in range(len(source_tuple[1]) - 1):         # each message in odometry message list
+	# each message in obstacle message list
+	for obs_index in range(len(source_tuple[0])):
+		# each message in odometry message list            
+		for odo_index in range(len(source_tuple[1]) - 1):    
 			a = source_tuple[1][odo_index].header.timestamp_sec
 			b = source_tuple[1][odo_index + 1].header.timestamp_sec
 			c = source_tuple[0][obs_index].header.timestamp_sec
@@ -45,71 +51,75 @@ def write_info(source_tuple, filename):
 			elif c == b:
 				proportion = 1		
 		localization_former = source_tuple[1][odo_index].localization.position
-		localization_next   = source_tuple[1][odo_index + 1].localization.position
-		
+		localization_next   = source_tuple[1][odo_index + 1].localization.position		
 		left_lane_marker    = left_lane_marker_group.lane_marker.add()
-		left_c0_position               = source_tuple[0][obs_index].lane_marker.left_lane_marker.c0_position
-		left_c1_heading_angle          = source_tuple[0][obs_index].lane_marker.left_lane_marker.c1_heading_angle
-		left_c2_curvature              = source_tuple[0][obs_index].lane_marker.left_lane_marker.c2_curvature
-		left_c3_curvature_derivative   = source_tuple[0][obs_index].lane_marker.left_lane_marker.c3_curvature_derivative
+		left_c0_position    = (source_tuple[0][obs_index].lane_marker
+			.left_lane_marker.c0_position)
+		left_c1_heading_angle          = (source_tuple[0][obs_index].lane_marker
+			.left_lane_marker.c1_heading_angle)
+		left_c2_curvature              = (source_tuple[0][obs_index].lane_marker
+			.left_lane_marker.c2_curvature)
+		left_c3_curvature_derivative   = (source_tuple[0][obs_index].lane_marker
+			.left_lane_marker.c3_curvature_derivative)
 		#the endpoint shift 1m from start point in x direction 
 		left_endpoint_xshift           = 1  
 		#the endpoint shift left_endpoint_yshift from start point in y direction accordingly
-		left_endpoint_yshift           = left_c0_position + left_c1_heading_angle + left_c2_curvature + left_c3_curvature_derivative
-		left_lane_marker.start_position.x              = localization_former.x * proportion + localization_next.x * (1 - proportion) 
-		left_lane_marker.start_position.y              = localization_former.y * proportion + localization_next.y * (1 - proportion) + left_c0_position
-		left_lane_marker.start_position.z              = localization_former.z * proportion + localization_next.z * (1 - proportion)
-		left_lane_marker.end_position.x                = localization_former.x * proportion + localization_next.x * (1 - proportion) + left_endpoint_xshift
-		left_lane_marker.end_position.y                = localization_former.y * proportion + localization_next.y * (1 - proportion) + left_endpoint_yshift
-		left_lane_marker.end_position.z                = localization_former.z * proportion + localization_next.z * (1 - proportion)
-		left_lane_marker.c0_position                   = left_c0_position
-		left_lane_marker.c1_heading_angle              = left_c1_heading_angle
-		left_lane_marker.c2_curvature                  = left_c2_curvature
-		left_lane_marker.c3_curvature_derivative       = left_c3_curvature_derivative
+		left_endpoint_yshift                = (left_c0_position + left_c1_heading_angle 
+			+ left_c2_curvature + left_c3_curvature_derivative)
+		left_lane_marker.start_position.x   = (localization_former.x * proportion 
+			+ localization_next.x * (1 - proportion)) 
+		left_lane_marker.start_position.y   = (localization_former.y * proportion 
+			+ localization_next.y * (1 - proportion) + left_c0_position)
+		left_lane_marker.start_position.z   = (localization_former.z * proportion 
+			+ localization_next.z * (1 - proportion))
+		left_lane_marker.end_position.x     = (localization_former.x * proportion 
+			+ localization_next.x * (1 - proportion) + left_endpoint_xshift)
+		left_lane_marker.end_position.y     = (localization_former.y * proportion 
+			+ localization_next.y * (1 - proportion) + left_endpoint_yshift)
+		left_lane_marker.end_position.z     = (localization_former.z * proportion 
+			+ localization_next.z * (1 - proportion))
+		left_lane_marker.c0_position        = left_c0_position
+		left_lane_marker.c1_heading_angle   = left_c1_heading_angle
+		left_lane_marker.c2_curvature       = left_c2_curvature
+		left_lane_marker.c3_curvature_derivative = left_c3_curvature_derivative
 		
 		right_lane_marker = right_lane_marker_group.lane_marker.add()
-		right_c0_position              = source_tuple[0][obs_index].lane_marker.right_lane_marker.c0_position
-		right_c1_heading_angle         = source_tuple[0][obs_index].lane_marker.right_lane_marker.c1_heading_angle
-		right_c2_curvature             = source_tuple[0][obs_index].lane_marker.right_lane_marker.c2_curvature
-		right_c3_curvature_derivative  = source_tuple[0][obs_index].lane_marker.right_lane_marker.c3_curvature_derivative
+		right_c0_position              = (source_tuple[0][obs_index].lane_marker
+			.right_lane_marker.c0_position)
+		right_c1_heading_angle         = (source_tuple[0][obs_index].lane_marker
+			.right_lane_marker.c1_heading_angle)
+		right_c2_curvature             = (source_tuple[0][obs_index].lane_marker
+			.right_lane_marker.c2_curvature)
+		right_c3_curvature_derivative  = (source_tuple[0][obs_index].lane_marker
+			.right_lane_marker.c3_curvature_derivative)
 		#the endpoint shift 1m from start point in x direction 
 		right_endpoint_xshift          = 1
 		#the endpoint shift left_endpoint_yshift from start point in y direction accordingly
-		right_endpoint_yshift          = right_c0_position + right_c1_heading_angle + right_c2_curvature + right_c3_curvature_derivative		
-		right_lane_marker.start_position.x            = localization_former.x * proportion + localization_next.x * (1 - proportion) 
-		right_lane_marker.start_position.y            = localization_former.y * proportion + localization_next.y * (1 - proportion) + right_c0_position
-		right_lane_marker.start_position.z            = localization_former.z * proportion + localization_next.z * (1 - proportion)
-		right_lane_marker.end_position.x              = localization_former.x * proportion + localization_next.x * (1 - proportion) + right_endpoint_xshift
-		right_lane_marker.end_position.y              = localization_former.y * proportion + localization_next.y * (1 - proportion) + right_endpoint_yshift
-		right_lane_marker.end_position.z              = localization_former.z * proportion + localization_next.z * (1 - proportion)		
+		right_endpoint_yshift          = (right_c0_position + right_c1_heading_angle
+			+ right_c2_curvature + right_c3_curvature_derivative)		
+		right_lane_marker.start_position.x            = (localization_former.x * proportion 
+			+ localization_next.x * (1 - proportion))
+		right_lane_marker.start_position.y            = (localization_former.y * proportion 
+			+ localization_next.y * (1 - proportion) + right_c0_position)
+		right_lane_marker.start_position.z            = (localization_former.z * proportion 
+			+ localization_next.z * (1 - proportion))
+		right_lane_marker.end_position.x              = (localization_former.x * proportion
+	   		+ localization_next.x * (1 - proportion) + right_endpoint_xshift)
+		right_lane_marker.end_position.y              = (localization_former.y * proportion
+			+ localization_next.y * (1 - proportion) + right_endpoint_yshift)
+		right_lane_marker.end_position.z              = (localization_former.z * proportion
+			+ localization_next.z * (1 - proportion))	
 		right_lane_marker.c0_position                 = right_c0_position
 		right_lane_marker.c1_heading_angle            = right_c1_heading_angle
 		right_lane_marker.c2_curvature                = right_c2_curvature
-		right_lane_marker.c3_curvature_derivative     = right_c3_curvature_derivative
-		
+		right_lane_marker.c3_curvature_derivative     = right_c3_curvature_derivative		
 	f.write(odometry_lane_markers_pack.SerializeToString())
-	f.close();
-
-def parse_info(filename):
-	"""Parse messages from protobuf message file 
-	Args: 
-		filename: protobuf message file name
-	"""
-	f = open(filename, "rb")
-	parsed_odometry_lane_markers_pack = odometry_lane_marker_pb2.OdometryLaneMarkersPack()
-	parsed_odometry_lane_markers_pack.ParseFromString(f.read())
-	print(type(parsed_odometry_lane_markers_pack.lane_markers[0]))
-	print(type(parsed_odometry_lane_markers_pack.lane_markers[0].lane_marker))
-	print(type(parsed_odometry_lane_markers_pack.lane_markers[0].lane_marker[0]))
-	print(type(parsed_odometry_lane_markers_pack.lane_markers[1]))
-	print(type(parsed_odometry_lane_markers_pack.lane_markers[1].lane_marker))
-	print(type(parsed_odometry_lane_markers_pack.lane_markers[1].lane_marker[0]))		
 	f.close()
-	
-if __name__ == '__main__':
+
+def main():
 	resource = read_bag('/apollo/data/bag/2018-09-20-11-14-59/2018-09-20-11-15-00_0.bag')
-	write_info(resource,"/apollo/modules/localization/testdata/OdometryLaneMarkers.pb")
-	parse_info("/apollo/modules/localization/testdata/OdometryLaneMarkers.pb")
+	write_info(resource,"/apollo/modules/localization/testdata/OdometryLaneMarkers.bin")
 
 
-
+if __name__ == '__main__':
+	main()
