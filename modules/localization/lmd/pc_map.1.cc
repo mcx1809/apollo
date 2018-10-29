ce40883dae6f9fab59a9c675995f1d8f37dfc0c5
xie.jf <xie.jf@cidi.ai>
2018-10-29 17:50:24 +0800

    *

---
+++ modules/localization/lmd/pc_map.1.cc
@@ -0,0 +1,224 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#include "modules/localization/lmd/pc_map.h"
+
+#include <limits>
+#include <utility>
+
+#include "modules/common/log.h"
+#include "modules/common/math/vec2d.h"
+
+namespace apollo {
+namespace localization {
+
+using apollo::common::Point3D;
+using apollo::common::PointENU;
+using apollo::common::Status;
+using apollo::common::math::RotateAxis;
+using apollo::common::math::Vec2d;
+using apollo::perception::LaneMarker;
+using apollo::perception::LaneMarkers;
+
+namespace {
+constexpr double kNodeSize = 1.0;
+constexpr double kMapResolution = 0.05;
+}  // namespace
+
+PCMap::PCMap(LMProvider* provider) {
+  CHECK_NOTNULL(provider);
+  provider_ = provider;
+
+  auto pack_size = provider_->GetLaneMarkerPackSize();
+  for (decltype(pack_size) pack_index = 0; pack_index < pack_size;
+       ++pack_index) {
+    auto lane_marker_size = provider_->GetLaneMarkerSize(pack_index);
+    for (decltype(lane_marker_size) lane_index = 0;
+         lane_index < lane_marker_size; ++lane_index) {
+      auto lane_marker =
+          provider_->GetLaneMarker(std::make_pair(pack_index, lane_index));
+      if (lane_marker != nullptr) LoadLaneMarker(*lane_marker);
+    }
+  }
+}
+
+Status PCMap::UpdateRange(const PointENU& position, double radius) {
+  return Status::OK();
+}
+
+const PCMapPoint* PCMap::GetNearestPoint(const PointENU& position,
+                                         double* d2) const {
+  auto node_index = MakeNodeIndex(position.x(), position.y());
+
+  auto node_it = nodes_.find(node_index);
+  if (node_it == nodes_.end()) return nullptr;
+
+  auto d2_min = std::numeric_limits<double>::max();
+  const PCMapPoint* nearest_point = nullptr;
+  for (const auto& point_pair : node_it->second.points) {
+    const auto& point = point_pair.second;
+    auto d2_testing = Vec2d(point.position.x(), point.position.y())
+                          .DistanceSquareTo(Vec2d(position.x(), position.y()));
+    if (d2_testing < d2_min) {
+      d2_min = d2_testing;
+      nearest_point = &point;
+    }
+  }
+
+  if (d2 != nullptr) *d2 = d2_min;
+
+  return nearest_point;
+}
+
+const PCMapPoint* PCMap::GetNearestPoint(const PointENU& position) const {
+  return GetNearestPoint(position, nullptr);
+}
+
+void PCMap::LoadLaneMarker(const OdometryLaneMarker& lane_marker) {
+  PCMapPoint* seg_point = nullptr;
+
+  for (const auto& lane_point : lane_marker.points()) {
+    auto node_index =
+        MakeNodeIndex(lane_point.position().x(), lane_point.position().y());
+
+    auto set_point = [&](std::map<Index2D, PCMapPoint>* points) {
+      Index2D index;
+      index.x = lane_point.position().x() / kMapResolution;
+      index.y = lane_point.position().y() / kMapResolution;
+
+      auto point_it = points->find(index);
+      if (point_it == points->end()) {
+        auto& point =
+            points->emplace(index, PCMapPoint(lane_point)).first->second;
+        if (seg_point != nullptr) {
+          seg_point->next = &point;
+          point.prev = seg_point;
+        }
+        seg_point = &point;
+      } else {
+        auto& point = point_it->second;
+        if (seg_point == nullptr) {
+          if (point.next == nullptr) seg_point = &point;
+        } else {
+          if (point.prev == nullptr) {
+            seg_point->next = &point;
+            point.prev = seg_point;
+          }
+          if (point.next == nullptr)
+            seg_point = &point;
+          else
+            seg_point = nullptr;
+        }
+      }
+    };
+
+    auto node_it = nodes_.find(node_index);
+    if (node_it == nodes_.end())
+      set_point(&nodes_[node_index].points);
+    else
+      set_point(&node_it->second.points);
+  }
+}
+
+const OdometryLaneMarker PCMap::GenerateOdometryLaneMarker(
+    const LaneMarker& lanemarker, const PointENU position, const double heading,
+    const double lane_length, const int point_number) const {
+  OdometryLaneMarker odo_lane;
+  for (int i = 0; i < point_number; ++i) {
+    auto point = odo_lane.add_points();
+    auto relative_x = lane_length / point_number * i;
+    auto relative_y = CalCurveValue(
+        relative_x, lanemarker.c0_position(), lanemarker.c1_heading_angle(),
+        lanemarker.c2_curvature(), lanemarker.c3_curvature_derivative());
+    auto curvature_value = CalCurvity(
+        relative_x, lanemarker.c0_position(), lanemarker.c1_heading_angle(),
+        lanemarker.c2_curvature(), lanemarker.c3_curvature_derivative());
+    point->set_curvature(curvature_value);
+    double enu_x, enu_y;
+    RotateAxis(-heading, relative_x, relative_y, &enu_x, &enu_y);
+    point->mutable_position()->set_x(position.x() + enu_x);
+    point->mutable_position()->set_y(position.y() + enu_y);
+    point->mutable_position()->set_z(position.z());
+    double enu_x_direct, enu_y_direct;
+    RotateAxis(-heading, 0, 1, &enu_x_direct, &enu_y_direct);
+    point->mutable_direct()->set_x(enu_x_direct);
+    point->mutable_direct()->set_y(enu_y_direct);
+    point->mutable_direct()->set_z(0.0);
+  }
+  return odo_lane;
+}
+
+std::vector<OdometryLaneMarker> PCMap::PrepareLaneMarkers(
+    const LaneMarkers& source, const PointENU position, const double heading,
+    const double lane_length, const int point_number) {
+  std::vector<OdometryLaneMarker> result;
+  if (source.has_left_lane_marker()) {
+    const auto& lanemarker = source.left_lane_marker();
+    const auto& odo_lane_marker = GenerateOdometryLaneMarker(
+        lanemarker, position, heading, lane_length, point_number);
+    result.emplace_back(odo_lane_marker);
+  }
+  if (source.has_right_lane_marker()) {
+    const auto& lanemarker = source.right_lane_marker();
+    const auto& odo_lane_marker = GenerateOdometryLaneMarker(
+        lanemarker, position, heading, lane_length, point_number);
+    result.emplace_back(odo_lane_marker);
+  }
+
+  for (int i = 0; i < source.next_left_lane_marker_size(); ++i) {
+    const auto& lanemarker = source.next_left_lane_marker(i);
+    const auto& odo_lane_marker = GenerateOdometryLaneMarker(
+        lanemarker, position, heading, lane_length, point_number);
+    result.emplace_back(odo_lane_marker);
+  }
+
+  for (int i = 0; i < source.next_right_lane_marker_size(); ++i) {
+    const auto& lanemarker = source.next_right_lane_marker(i);
+    const auto& odo_lane_marker = GenerateOdometryLaneMarker(
+        lanemarker, position, heading, lane_length, point_number);
+    result.emplace_back(odo_lane_marker);
+  }
+  return result;
+}
+
+double PCMap::CalCurveValue(const double x_value, const double c0,
+                            const double c1, const double c2,
+                            const double c3) const {
+  return c3 * pow(x_value, 3.0) + c2 * pow(x_value, 2.0) + c1 * x_value + c0;
+}
+
+double PCMap::CalDerivative(const double x_value, const double c0,
+                            const double c1, const double c2,
+                            const double c3) const {
+  return 3 * c3 * pow(x_value, 2.0) + 2 * c2 + c1;
+}
+
+double PCMap::CalCurvity(const double x_value, const double c0, const double c1,
+                         const double c2, const double c3) const {
+  double derivative = CalDerivative(x_value, c0, c1, c2, c3);
+  return abs(6 * c3 * x_value + 2 * c2) /
+         pow(1 + pow(derivative, 2.0), (3.0 / 2));
+}
+
+PCMap::Index2D PCMap::MakeNodeIndex(double x, double y) const {
+  Index2D index;
+  index.x = x / kNodeSize;
+  index.y = y / kNodeSize;
+  return index;
+}
+
+}  // namespace localization
+}  // namespace apollo
---
+++ modules/localization/lmd/pc_map.1.h
@@ -0,0 +1,220 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file pc_map.h
+ * @brief The class of PCMap.
+ */
+
+#ifndef MODULES_LOCALIZATION_LMD_PC_MAP_H_
+#define MODULES_LOCALIZATION_LMD_PC_MAP_H_
+
+#include <map>
+#include <vector>
+
+#include "modules/common/math/math_utils.h"
+#include "modules/common/proto/geometry.pb.h"
+#include "modules/common/status/status.h"
+
+#include "modules/localization/lmd/lm_provider.h"
+#include "modules/perception/proto/perception_obstacle.pb.h"
+
+/**
+ * @namespace apollo::localization
+ * @brief apollo::localization
+ */
+namespace apollo {
+namespace localization {
+
+/**
+ * @struct PCMapPoint
+ * @brief  Point stored in map.
+ */
+struct PCMapPoint {
+  PCMapPoint* prev = nullptr;
+  PCMapPoint* next = nullptr;
+  apollo::common::PointENU position;
+  apollo::common::Point3D direction;
+  double curvature;
+
+  explicit PCMapPoint(const OdometryLaneMarkerPoint& point) {
+    position = point.position();
+    direction = point.direct();
+    curvature = point.curvature();
+  }
+};
+
+/**
+ * @class PCMap
+ *
+ * @brief  Map of point cloud.
+ */
+class PCMap {
+  struct Node {
+    std::size_t prev;
+    std::size_t next;
+
+    std::size_t parent_index;
+    std::size_t lt_index;
+    std::size_t lb_index;
+    std::size_t rt_index;
+    std::size_t rb_index;
+
+    std::size_t point_index;
+    int level;
+  };
+
+  struct Index2D {
+    int64_t x;
+    int64_t y;
+
+    bool operator<(const Index2D& other) const {
+      if (x < other.x)
+        return true;
+      else if (x == other.x)
+        return y < other.y;
+      else
+        return false;
+    }
+  };
+
+  struct Node {
+    std::map<Index2D, PCMapPoint> points;
+  };
+
+ public:
+  explicit PCMap(LMProvider* provider);
+
+  /**
+   * @brief  Update map for range.
+   * @param position The position of center point.
+   * @param the radius.
+   * @return Status::OK() if a suitable speed-data is created; error otherwise.
+   */
+  apollo::common::Status UpdateRange(const apollo::common::PointENU& position,
+                                     double radius);
+
+  /**
+   * @brief  Find the nearest point in lane_marker according to the given
+   * position.
+   * @param position The given position.
+   * @param d2 Distance squqre.
+   * @return The nearest point in lane_marker or nullptr.
+   */
+  const PCMapPoint* GetNearestPoint(const apollo::common::PointENU& position,
+                                    double* d2) const;
+
+  /**
+   * @brief  Find the nearest point in lane_marker according to the given
+   * position.
+   * @param position The given position.
+   * @return The nearest point in lane_marker or nullptr.
+   */
+  const PCMapPoint* GetNearestPoint(
+      const apollo::common::PointENU& position) const;
+
+  /**
+   * @brief insert the points in the given OdometryLaneMarker to nodes.
+   * @param lane_marker The lane_marker to sample points
+   */
+  void LoadLaneMarker(const OdometryLaneMarker& lane_marker);
+
+  /**
+   * @brief  Prepare odometry lane markers according to the source perception
+   * lane_markers and given params
+   * @param source The source perception lane markers.
+   * @param position The ENU position after pc registration
+   * @param heading The heading value after pc registration
+   * @param lane_length The length to sample points
+   * @param point_number The total sample point number
+   * @return The vector contains the generated odometry lane markers
+   */
+  std::vector<OdometryLaneMarker> PrepareLaneMarkers(
+      const apollo::perception::LaneMarkers& source,
+      const apollo::common::PointENU position, const double heading,
+      const double lane_length, const int point_number);
+
+ private:
+  /**
+   *@brief Maker node index data struct according to given params
+   *@param x The x value to make node index
+   *@param y The y value to make node index
+   *@return The generated Index2D data struct by given params
+   */
+  Index2D MakeNodeIndex(const double x, const double y) const;
+
+  /**
+   * @brief  Generate odometry lane marker according to the source perception
+   * lane_marker and given params
+   * @param lanemarker The source perception lanemarker.
+   * @param position The ENU position after pc registration
+   * @param heading The heading value after pc registration
+   * @param lane_length The length to sample points
+   * @param point_number The total sample point number
+   * @return The generated odometry lane marker
+   */
+  const OdometryLaneMarker GenerateOdometryLaneMarker(
+      const apollo::perception::LaneMarker& lanemarker,
+      const apollo::common::PointENU position, const double heading,
+      const double lane_length, const int point_number) const;
+
+  /**
+   * @brief  Calculate curve value by given curve params.
+   * @param  x_value: value of x.
+   *         c0: position.
+   *         c1: heading_angle.
+   *         c2: curvature.
+   *         c3: curvature_derivative.
+   * @return y = c3 * x**3 + c2 * x**2 + c1 * x + c0.
+   */
+  double CalCurveValue(const double x_value, const double c0, const double c1,
+                       const double c2, const double c3) const;
+
+  /**
+   * @brief  Calculate the first derivative value according to x_value and curve
+   * analysis formula y = c3 * x**3 + c2 * x**2 + c1 * x + c0
+   * @param  x_value: value of x.
+   *         c0: position.
+   *         c1: heading_angle.
+   *         c2: curvature.
+   *         c3: curvature_derivative.
+   * @return the first derivative value when x equal to x_value
+   */
+  double CalDerivative(const double x_value, const double c0, const double c1,
+                       const double c2, const double c3) const;
+
+  /**
+    * @brief  Calculate the curvity value according to x_value and curve
+    analysis formula y = c3 * x**3 + c2 * x**2 + c1 * x + c0
+    * @param  x_value: value of x.
+    *         c0: position.
+    *         c1: heading_angle.
+    *         c2: curvature.
+    *         c3: curvature_derivative.
+    * @return K = |y''| / (1 + y'**2)**(3.0/2)
+            curvity_value K according to the analysis formula with x = x_value
+    */
+  double CalCurvity(const double x_value, const double c0, const double c1,
+                    const double c2, const double c3) const;
+
+ private:
+  LMProvider* provider_;
+};
+
+}  // namespace localization
+}  // namespace apollo
+
+#endif  // MODULES_LOCALIZATION_LMD_PC_MAP_H_
