b76c7ffe9efac25992bcc294f6a3dca26473ad9c
yujianyi <yujianyi1987@126.com>
2018-11-13 14:58:01 +0800

    localization:update the python draw code.

--- modules/tools/localization/readinfo_and_draw.py
+++ modules/tools/localization/readinfo_and_draw.py
@@ -10,6 +10,11 @@ stationerror= []
 lateralerror= []
 headingerror= []
 
+velocity_station_error = []
+velocity_lateral_error = []
+truevelocity_station = []
+truevelocity_lateral = []
+
 input = open('/apollo/data/log/localization.INFO', 'r')
 data=input.readlines()
 
@@ -29,7 +34,20 @@ for line1 in data:
         heading = line1[-1].split('[')[1].split(']')[0]
         headingerror.append(57.3*(float)(heading))
 
-plt.title('Error Analysis')
+for line3 in data:
+     if re.findall('True velocity',line3):
+        line3 = line3.split(',')
+        true_x = line3[1].split('[')[1].split(']')[0]
+        true_y = line3[2].split('[')[1].split(']')[0]
+        error_x = line3[4].split('[')[1].split(']')[0]
+        error_y = line3[5].split('[')[1].split(']')[0]
+        velocity_station_error.append(error_x)
+        velocity_lateral_error.append(error_y)
+        truevelocity_station.append(true_x)
+        truevelocity_lateral.append(true_y)
+
+plt.figure('Position Error')
+plt.title('Position Error Analysis')
 plt.plot(time,stationerror,color='blue', label='$station$')
 plt.plot(time,lateralerror,color='red',label='$lateral$')
 plt.plot(time,headingerror,color='black',label='$heading$')
@@ -39,6 +57,18 @@ plt.legend()
 plt.xlabel('time(s)')
 plt.ylabel('error(m)')
 #plt.show()
+
+plt.figure('Velocity Error')
+plt.title('Velocity Error Analysis')
+plt.plot(time,velocity_station_error,color='blue', label='$StationError$')
+plt.plot(time,velocity_lateral_error,color='red',label='$LateralError$')
+#plt.plot(time,truevelocity_station,color='black',label='$TrueStation$')
+#plt.plot(time,truevelocity_lateral,color='green',label='$TrueLateral$')
+plt.legend()
+#设置坐标轴名称
+plt.xlabel('time(s)')
+plt.ylabel('error(m/s)')
+plt.show()
 
 #画gps图
 trueX = []
@@ -60,4 +90,4 @@ plt.figure('GPSY')
 plt.plot(time,trueY, '.b',)
 plt.plot(time,trueY,'r')
 
-plt.show()
+#plt.show()
